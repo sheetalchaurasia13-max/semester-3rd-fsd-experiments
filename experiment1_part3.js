84b5c993e9870a8eef1d16cd92ff55614f57b7b3
sheetalchaurasia13-max <sheetalchaurasia13@gmail.com>
2026-09-11 12:07:15 +0530

    change

--- experiment1_part3.js
+++ experiment1_part3.js
@@ -4,6 +4,6 @@ process.nextTick(() => console.log('2. nextTick'));
 
 setTimeout(() => console.log('3. setTimeout') , 0);
 
-setImmediate(() => console.log('2.  setImmediate'));
+setImmediate(() => console.log('4.  setImmediate'));
 
  console.log('5. End');
